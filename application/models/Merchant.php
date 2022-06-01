af3be12a5ae6f69817bf9a5f9e9480d281e643f9
Rehan <rehanmasiya786@gmail.com>
2022-06-01 15:26:01 +0530

    Add Transaction API - Rehan

--- application/models/Merchant.php
+++ application/models/Merchant.php
@@ -258,9 +258,10 @@ class Merchant extends CI_Model
 
      public function ProductDetails($product_id,$brand_id) {
 
-            $this->db->select('`products`.*, CONCAT("'.base_url().'", `products`.product_image) as product_image');
+            $this->db->select('`products`.*, CONCAT("'.base_url().'", `products`.product_image) as product_image,`merchants.physical_address` as physical_address , `merchants.province` as province');
             $this->db->where('merchant_id',$brand_id);
             $this->db->where('id',$product_id);
+            $this->db->join('merchants', 'merchants.id = products.merchant_id');
             $query = $this->db->get('products');
             $items = $query->result_array();
             if(!empty($items))
