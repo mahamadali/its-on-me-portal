98b75ac2937d7fe3057e0808f8841863e4d91b68
Mahamad Ali <manknojiya121@gmail.com>
2022-06-02 18:32:27 +0530

    user transactions api - Ak

--- application/models/User.php
+++ application/models/User.php
@@ -209,7 +209,7 @@ class User extends CI_Model
 
 
     public function getTransactionsByMerchant($id) {
-        $this->db->select('transactions.id as transaction_id,transactions.created_at as date of order,transactions.price as amount,merchants.username as brand_name, transactions.full_name as receiver_name');
+        $this->db->select('transactions.id as transaction_id,transactions.created_at as date_of_order,transactions.price as amount,merchants.username as brand_name, transactions.full_name as receiver_name');
         $this->db->where('merchant_id', $id);
         $this->db->where('transactions.status !=', 'PENDING');
         $this->db->join('merchants', 'merchants.id = transactions.merchant_id');
@@ -219,7 +219,7 @@ class User extends CI_Model
         }
         else
         {
-             return '[]';
+             return [];
         }
     }
 
@@ -233,7 +233,7 @@ class User extends CI_Model
         }
         else
         {
-             return '[]';
+             return [];
         }
     }
 
