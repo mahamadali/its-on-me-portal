c28e4b3412878cb5e3373e93f0d839be06237a5b
Rehan <rehanmasiya786@gmail.com>
2022-07-06 11:32:39 +0530

    Addd Brands Module and Add for merchant  -   Rehan

--- application/views/brands/index.php
+++ application/views/brands/index.php
@@ -7,7 +7,7 @@
               <!-- <h6 class="h2 text-white d-inline-block mb-0">Products</h6> -->
               <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                 <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
-                  <li class="breadcrumb-item active"><a href="#"><i class="fas fa-home"></i></a>&nbsp;All Product Categories</li>
+                  <li class="breadcrumb-item active"><a href="#"><i class="fas fa-home"></i></a>&nbsp;All Brands</li>
                 </ol>
               </nav>
             </div>
