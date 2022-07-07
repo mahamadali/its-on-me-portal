85f4b67146219cfa7339686c76ec0d934ea4ebc5
Rehan <rehanmasiya786@gmail.com>
2022-07-07 14:00:06 +0530

    Add brand info in merchant API  -   Rehan

--- application/controllers/api/General.php
+++ application/controllers/api/General.php
@@ -70,46 +70,39 @@ class General extends REST_Controller {
     }
 }
 
-public function generatePaymentLinkViaPaygate($data) {
+public function generatePaymentLinkViaPaygate($data = '') {
 
-    $encryptionKey = 'secret';
+   $encryptionKey = 'secret';
 
-    $DateTime = new DateTime();
+$data = array(
+    'PAYGATE_ID'        => 10011072130,
+    'PAY_REQUEST_ID'    => '23B785AE-C96C-32AF-4879-D2C9363DB6E8',
+    'REFERENCE'         => 'pgtest_123456789'
+);
 
-    $data = array(
-        'PAYGATE_ID'        => 10011072130,
-        'REFERENCE'         => 'pgtest_123456789',
-        'AMOUNT'            => 3299,
-        'CURRENCY'          => 'ZAR',
-        'RETURN_URL'        => 'https://my.return.url/page',
-        'TRANSACTION_DATE'  => $DateTime->format('Y-m-d H:i:s'),
-        'LOCALE'            => 'en-za',
-        'COUNTRY'           => 'ZAF',
-        'EMAIL'             => 'customer@paygate.co.za',
-    );
+print_r($data);exit();
+$checksum = md5(implode('', $data) . $encryptionKey);
 
-    $checksum = md5(implode('', $data) . $encryptionKey);
+$data['CHECKSUM'] = $checksum;
 
-    $data['CHECKSUM'] = $checksum;
-
-    $fieldsString = http_build_query($data);
+$fieldsString = http_build_query($data);
 
 //open connection
-    $ch = curl_init();
+$ch = curl_init();
 
 //set the url, number of POST vars, POST data
-    curl_setopt($ch, CURLOPT_URL, 'https://secure.paygate.co.za/payweb3/initiate.trans');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_NOBODY, false);
-    curl_setopt($ch, CURLOPT_REFERER, $_SERVER['HTTP_HOST']);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldsString);
+curl_setopt($ch, CURLOPT_URL, 'https://secure.paygate.co.za/payweb3/query.trans');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_NOBODY, false);
+curl_setopt($ch, CURLOPT_REFERER, $_SERVER['HTTP_HOST']);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldsString);
 
 //execute post
-    $result = curl_exec($ch);
+$result = curl_exec($ch);
 
 //close connection
-    curl_close($ch);
+curl_close($ch);
 }
 
 
