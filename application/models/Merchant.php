0612db141eab8843c60f9380304e470d2aa7b3cc
Rehan <rehanmasiya786@gmail.com>
2022-07-07 13:27:05 +0530

    Add brand info in merchant API  -   Rehan

--- application/models/Merchant.php
+++ application/models/Merchant.php
@@ -154,7 +154,6 @@ class Merchant extends CI_Model
         $this->db->join('brands', 'brands.id = merchants.brand_id');
         $this->db->where('merchants`.status', 1);
         $query = $this->db->get('merchants');
-        print_r($this->db->last_query());exit();
         $result = $query->result_array();
         return $result;
     }
