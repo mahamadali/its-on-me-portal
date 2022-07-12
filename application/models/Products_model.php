2df5656eeda29cf2419e9c0335d93672eb0ebbae
Rehan <rehanmasiya786@gmail.com>
2022-07-12 12:57:34 +0530

    search by brand APi changed -   Rehan

--- application/models/Products_model.php
+++ application/models/Products_model.php
@@ -442,7 +442,7 @@ class Products_model extends CI_Model
         public function searchByBrand($merchantName)
         {
             /*if(empty($merchantName)) { return []; }*/
-            $this->db->select('`merchants`.*,CONCAT("'.base_url().'", `merchants`.profile_picture) as merchant_image,merchants.is_super_merchant as is_super_merchant, provinces.name as province_name');
+            $this->db->select('`merchants`.*,CONCAT("'.base_url().'", `merchants`.profile_picture) as profile_picture,merchants.is_super_merchant as is_super_merchant, provinces.name as province_name');
             $this->db->join('provinces', 'provinces.id=merchants.province');
             if(!empty($merchantName))
             {
