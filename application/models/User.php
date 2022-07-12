e81b8631c5d9fb44c5128da3f25b2eea84721668
Rehan <rehanmasiya786@gmail.com>
2022-07-12 19:03:07 +0530

    search by brand APi changed -   Rehan

--- application/models/User.php
+++ application/models/User.php
@@ -248,7 +248,7 @@ class User extends CI_Model
     public function getSuccessTransactionData($id,$transaction_id) {
         $this->db->select('*');
          $this->db->where('user_id', $id);
-         $this->db->where('transaction_id', $transaction_id);
+         $this->db->where('id', $transaction_id);
          $query = $this->db->get('transactions');
         if($query->num_rows() > 0) {
             return $query->row();
